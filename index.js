6f57f4d9eff6402143b0ab78478a456d03a5ea9d
Prathamesh <prathameshtheurkar037@gmail.com>
2023-10-23 19:58:27 +0530

    Added

---
+++ index.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('input-box');
+
+const btn = document.getElementById('add-btn');
+
+btn.addEventListener('click' , ()=>{
+   
+    console.log(input.value);
+})
